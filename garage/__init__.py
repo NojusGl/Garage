7d73b9db6fac6bd5305e3b3a6883de86196cfb7d
NojusGl <99596734+NojusGl@users.noreply.github.com>
2024-10-08 23:14:14 +0300

    finished (maybe)
    1) implementuotos likusios funkcijos;
    2) nebenaudojamas garažo registras (jį iš esmės reiktų naudoti tik užregistruojant garažą, o ten jis nebuvo naudojamas, tai ir nebekišau);
    3) spots saugomi taip, būtų galima turėti kelis garažus su tais pačiais spot numeriais (pakeista spotKey() funkcija);
    4) iš Flask'o importuota Response (nes kitaip man nėjo normaliai išdėlioti field'ų, grąžinant visą garažo informaciją);
    5) atnaujinta vietos užregistravimo funkcija (jei vieta jau užimta, naujos number plate jau nebegalima įrašyti).

--- garage/__init__.py
+++ garage/__init__.py
@@ -1,12 +1,12 @@
 import re
 import werkzeug as tool
 import redis
-from flask import (Flask, request, jsonify, abort)
-# this is my comment
+from flask import (Flask, request, Response, jsonify, abort)
+from collections import OrderedDict
 
 #TODO specify address regex
 addressRegex = ""
-garageRegex = "^g[0-9]" #NOTE Do we need it? Probably
+# garageRegex = "^g[0-9]" # Do we need it?
 licenseRegex = "^[A-Z0-9]{1,7}$"
 
 def create_app():
@@ -16,8 +16,8 @@ def create_app():
     def garageKey(garageNo):
         return f'Garage:{garageNo}'
     
-    def spotKey(spotNo):
-        return f'Spot:{spotNo}'
+    def spotKey(garageNo, spotNo):
+        return f'Spot:{garageNo}:{spotNo}'
 
     #Užregistruoti garažą.
     @app.route('/garage', methods=['PUT'])
@@ -39,79 +39,142 @@ def create_app():
         
     #Gauti garažo informaciją.
     @app.route('/garage/<garageId>', methods=['GET'])
-    def get_garage():
-        pass
+    def get_garage(garageId):
+        garage = redisClient.get(garageKey(garageId))
+        if (garage != None):
+            garageInfo = garage.split(':')
+            
+            response = (
+                f'{{"id": "{garageInfo[0]}",'
+                f'"spots": {int(garageInfo[1])},'
+                f'"address": "{garageInfo[2]}"}}'
+            )
+            return Response(response, mimetype='application/json'), 200
+        else: 
+            return {"message": "Garage not found"}, 404
     
     #Gauti bendrą vietų skaičių garaže.
     @app.route('/garage/<garageId>/configuration/spots', methods=['GET'])
     def get_spots(garageId):
-        if re.search(garageRegex, garageId) != None:
-            garage = redisClient.get(garageKey(garageId))
-            if (garage != None):
-                garageInfo = garage.split(':')
+        garage = redisClient.get(garageKey(garageId))
+        if (garage != None):
+            garageInfo = garage.split(':')
 
-                return {
-                    "spots": int(garageInfo[1])
-                }
-            else: 
-                return {"message": "Garage not found"}, 404
-        return {"message": "Invalid garageId"}, 400
+            return {
+                "spots": int(garageInfo[1])
+            }, 200
+        else: 
+            return {"message": "Garage not found"}, 404
     
     #Pakeisti garažo vietų skaičių.
     @app.route('/garage/<garageId>/configuration/spots', methods=['POST'])
-    def update_garage_spots():
-        pass
+    def update_garage_spots(garageId):
+        reqBody = request.json
+        spots = int(reqBody.get("spots"))
+        if spots != None:
+            if spots > 0:
+                garage = redisClient.get(garageKey(garageId))
+                if garage != None:
+                    garageInfo = garage.split(':')
+                    old_spots = int(garageInfo[1])
+                    if (spots >= old_spots):
+                        garageValue = f"{garageInfo[0]}:{spots}:{garageInfo[2]}"
+                        redisClient.set(garageKey(garageId), garageValue)
+                        return "", 200
+                    else:
+                        for spotNo in range(spots, old_spots + 1):
+                            license = redisClient.get(spotKey(garageId, spotNo))
+                            if license != None:
+                                return {"message": f'spot {spotNo} is not empty'}, 400
+                        return "", 200
+                else:
+                    return {"message": "Garage not found"}, 404
+            else:
+                return {"message": "Number of spots should be higher than 0"}, 400
+        else:
+            return {"message": "Invalid request body"}, 400
+        
+        # padidinti visad galiu
+        # jei noriu sumažinti:
+            # pasiimu tą range nuo to, iki kurio 
+            # jei randu bent vieną užimtą spot'ą, sakau, kad ta vieta yra užimta
+            # 
 
     #Užregistruoti užimtą vietą garaže.
     @app.route('/garage/<garageId>/spots/<spotNo>', methods=['POST'])
     def update_spot(garageId, spotNo):
-        if re.search(garageRegex, garageId) != None:
-            garage = redisClient.get(garageKey(garageId))
-            if (garage != None):
-                garageInfo = garage.split(':') 
-                spots = int(garageInfo[1])
-                if int(spotNo) <= spots and int(spotNo) > 0:
+        garage = redisClient.get(garageKey(garageId))
+        if (garage != None):
+            garageInfo = garage.split(':') 
+            spots = int(garageInfo[1])
+            if int(spotNo) <= spots and int(spotNo) > 0:
+                str, num = get_spot_license(garageId, spotNo)
+                if (num == 204):
                     reqBody = request.json
                     license = reqBody.get("licenseNo")
-                    redisClient.set(spotKey(spotNo), license)
+                    redisClient.set(spotKey(garageInfo[0], spotNo), license)
                     return ''
                 else:
-                    return {"message": "Spot number not found"}, 404
-            else: 
-                return {"message": "Garage not found"}, 404
-        return {"message": "Invalid garageId"}, 400
+                    return {"message": "Spot is already taken"}, 400
+            else:
+                return {"message": "Spot number not found"}, 404
+        else: 
+            return {"message": "Garage not found"}, 404
     
     #Gauti automoblio numerį, kuris užima vietą.
     @app.route('/garage/<garageId>/spots/<spotNo>', methods=['GET'])
-    def get_spot_license():
-        pass
-    
+    def get_spot_license(garageId, spotNo):
+        garage = redisClient.get(garageKey(garageId))
+        if (garage != None):
+            garageInfo = garage.split(':') 
+            spots = int(garageInfo[1])
+            if int(spotNo) <= spots and int(spotNo) > 0:
+                license = redisClient.get(spotKey(garageId, spotNo))
+                if (license != None):
+                    return license, 200
+                else:
+                    return {"message": "Spot is free"}, 204
+            else:
+                return {"message": "Spot number not found"}, 404
+        else: 
+            return {"message": "Garage not found"}, 404
+
     #Pažymėti vietą laisva.
     @app.route('/garage/<garageId>/spots/<spotNo>', methods=['DELETE'])
-    def delete_spot_license():
-        pass
+    def delete_spot_license(garageId, spotNo):
+        garage = redisClient.get(garageKey(garageId))
+        if (garage != None):
+            garageInfo = garage.split(':') 
+            spots = int(garageInfo[1])
+            if int(spotNo) <= spots and int(spotNo) > 0:
+                redisClient.delete(spotKey(garageId, spotNo))
+                return '', 200
+            else:
+                return {"message": "Spot number not found"}, 404
+        else: 
+            return {"message": "Garage not found"}, 404
     
     #Gauti laisvų ir užimtų vietų skaičių garaže.
     @app.route('/garage/<garageId>/status', methods=['GET'])
     def get_garage_status(garageId):
-        if re.search(garageRegex, garageId) != None:
-            garage = redisClient.get(garageKey(garageId))
-            if (garage != None):
-                garageInfo = garage.split(':') 
-                spots = int(garageInfo[1])
-                freeSpots = 0
-                occupiedSpots = 0
-                for spotNo in range(1, spots+1):
-                    spot = redisClient.get(spotKey(spotNo))
-                    if spot == None: freeSpots+=1
-                    else: occupiedSpots+=1
+        garage = redisClient.get(garageKey(garageId))
+        if (garage != None):
+            garageInfo = garage.split(':') 
+            spots = int(garageInfo[1])
+            freeSpots = 0
+            occupiedSpots = 0
+            for spotNo in range(1, spots+1):
+                license = redisClient.get(spotKey(garageId, spotNo))
+                if license == None:
+                    freeSpots += 1
+                else:
+                    occupiedSpots += 1
 
-                return {
-                    "freeSpots": freeSpots,
-                    "occupiedSpots": occupiedSpots
-                }
-            else: 
-                return {"message": "Garage not found"}, 404
-        return {"message": "Invalid garageId"}, 400
+            return {
+                "freeSpots": freeSpots,
+                "occupiedSpots": occupiedSpots
+            }
+        else: 
+            return {"message": "Garage not found"}, 404
 
     return app
