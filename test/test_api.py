bea992c7593e52b3893b1f2acea8a8d77ff5a88c
NojusGl <99596734+NojusGl@users.noreply.github.com>
2024-10-13 21:14:59 +0300

    final (?) touches

--- test/test_api.py
+++ test/test_api.py
@@ -122,9 +122,3 @@ def get_garage_status(garage_id, assrt = True):
     assert response.status_code == 200
 
   return response
-
-if __name__ == "__main__":
-    test_registering_and_getting_garage()
-    test_garage_configuration()
-    test_occupying_spots()
-    print ("tests have been passed")
